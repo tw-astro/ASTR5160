2c6cba09e4235d8c280444c55a3c26b79b762517
tw-astro <weinbeck@alum.mit.edu>
2025-01-31 13:39:03 -0700

    Create my-program.py

---
+++ week1/my-program.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Create a function that just says hi
+def myprogram():
+        print("Hello world!")
+
+
+# Run the convivial function
+if name == "__main__":
+        myprogram()
