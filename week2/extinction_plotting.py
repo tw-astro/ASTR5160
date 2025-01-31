7bca6566051a609625e0fb2a9a1047f9cb9b3283
tw-astro <weinbeck@alum.mit.edu>
2025-01-31 15:20:52 -0700

    Update extinction_plotting.py

--- week2/extinction_plotting.py
+++ week2/extinction_plotting.py
@@ -20,18 +20,21 @@ def read_table():
     # Print the column names
     print( objs.colnames )
     
+    # Plot RA vs Dec for all sources
     plt.scatter( objs['RA'], objs['DEC'], marker ='+', c='b', alpha=0.5, \
                  label='All sources' )
-
     plt.xlabel('Right Ascension (degs)')
     plt.ylabel('Declination (degs)')
 
-    
+
+    # Boolean indexing: identify only sources with higher extinction    
     reddened = objs['EXTINCTION'][:,0] > 0.22
     
+    # Overplot these sources in red
     plt.scatter( objs[reddened]['RA'], objs[reddened]['DEC'], \
                  marker ='+', c='r', alpha=0.5, label='High extinction' )
     plt.legend()
+    # Save figure as PNG
     plt.savefig( class_path +'struc-RA-dec.png', dpi=300 )
     plt.show()
 
