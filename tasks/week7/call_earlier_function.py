f696ecfec7e4e950a4a3f87287727002f97a8aa0
Tony Weinbeck <weinbeck@alum.mit.edu>
2025-03-31 16:19:42 -0600

    week7_changes

---
+++ tasks/week7/call_earlier_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+    
+from tasks.week2.sky_coord_converter import get_coords
+
+if __name__ == '__main__':
+    name = 'M57'
+    get_coords( name )
+
