91356d4a8b6e22a5b1508c30e349f7509a2a1bbf
tw-astro <weinbeck@alum.mit.edu>
2025-05-16 13:30:08 -0600

    Add files via upload

--- tasks/week7/call_earlier_function.py
+++ tasks/week7/call_earlier_function.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-    
-from tasks.week2.sky_coord_converter import get_coords
+
+# Import task from previous week    
+from week2.sky_coord_converter import get_coords
+
 
 if __name__ == '__main__':
+    # Just checking that we can successfully import modules from other directories
     name = 'M57'
     get_coords( name )
 
